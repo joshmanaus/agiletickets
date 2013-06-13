0ba98e9b2814f861fd6e900a0289cd72b706cbde
joseph <joshmanaus@gmail.com>
2013-06-12 21:09:18 -0300

    commitando

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,33 +1,49 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
+	Sessao sessao;
 
-	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
-
-        Assert.assertTrue(sessao.podeReservar(1));
+	@Before
+	public void criarSessao() {
+		this.sessao = new Sessao();
 	}
 
 	@Test
-	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+	public void deve_Vender_Ingresso_Se_Quantidade_Disponivel_Maior_Que_Quantidade_Solicitada()
+			throws Exception {
+
+		sessao.setTotalIngressos(2);
+
+		Assert.assertTrue(sessao.podeReservar(1));
+	}
+
+	@Test
+	public void nao_Deve_Vender_Ingresso_Se_Quantidade_Disponivel_Insuficiente()
+			throws Exception {
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
 	}
 
 	@Test
-	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
+	public void reservar_Ingressos_Deve_Diminuir_O_Numero_De_Ingressos_Disponiveis()
+			throws Exception {
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
+	}
+
+	@Test
+	public void reservar_Quantidade_Igual_Disponivel() {
+		sessao.setTotalIngressos(2);
+
+		Assert.assertTrue(sessao.podeReservar(2));
+
 	}
 }
