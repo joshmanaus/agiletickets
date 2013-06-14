4b2f9250b86e9a0f44eaf59e02dba722a7411588
joseph <joshmanaus@gmail.com>
2013-06-14 19:59:41 -0300

    teste jenkins

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -75,7 +75,7 @@ public class EspetaculoTest {
 
 	private Sessao sessaoComIngressosSobrando(int quantidade) {
 		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(quantidade * 2);
+//		sessao.setTotalIngressos(quantidade * 2);
 		sessao.setIngressosReservados(quantidade);
 
 		return sessao;
